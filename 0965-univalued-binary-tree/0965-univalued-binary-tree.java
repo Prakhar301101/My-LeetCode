ee726be58767848ec3d1929fba4a66a0156bedbe
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2022-10-17 19:38:07 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (14.23%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.java
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -14,29 +14,48 @@
  * }
  */
 class Solution {
+    int c;
+    boolean flag=true;
+    
     public boolean isUnivalTree(TreeNode root) {
+//         if(root==null){
+//             return true;
+//         }
+//         List<Integer> t=new ArrayList<>();
+//         t=preOrder(root);
+//         for(int i=0;i<t.size()-1;i++){
+//             if(t.get(i)!=t.get(i+1)){
+//                 return false;
+//             }
+//         }
+//         return true;
+        
+        
+//     }
+//     static List<Integer> preOrder(TreeNode root){
+//         List<Integer> a=new ArrayList<>();
+//         if(root==null){
+//             return a;
+//         }
+//         a.add(root.val);
+//         a.addAll(preOrder(root.left));
+//         a.addAll(preOrder(root.right));
+//         return a;
+//     } 
+        
+    c=root.val;
+    check(root);
+     return flag;   
+}
+     void check(TreeNode root){
         if(root==null){
-            return true;
+            return;
         }
-        List<Integer> t=new ArrayList<>();
-        t=preOrder(root);
-        for(int i=0;i<t.size()-1;i++){
-            if(t.get(i)!=t.get(i+1)){
-                return false;
-            }
-        }
-        return true;
-        
-        
+        if(c!=root.val)
+            flag=false;
+        check(root.left);
+        check(root.right);
     }
-    static List<Integer> preOrder(TreeNode root){
-        List<Integer> a=new ArrayList<>();
-        if(root==null){
-            return a;
-        }
-        a.add(root.val);
-        a.addAll(preOrder(root.left));
-        a.addAll(preOrder(root.right));
-        return a;
-    } 
+    
+    
 }
