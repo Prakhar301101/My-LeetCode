c636033db7d406812d4daefc2646b2ba752f6197
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2022-10-23 10:30:45 +0530

    Time: 12 ms (49.03%), Space: 45.1 MB (33.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int a[]=new int[26];
-        int b[]=new int[26];
-        a=freq(s);
-        b=freq(t);
+        char a[]=s.toCharArray();
+        char b[]=t.toCharArray();
         if(a.length!=b.length){
             return false;
         }
-        for(int i=0;i<26;i++){
+        Arrays.sort(a);Arrays.sort(b);
+        for(int i=0;i<a.length;i++){
             if(a[i]!=b[i]){
                 return false;
             }
         }
         return true;
+        
     }
      static int[] freq(String s){
         int f[]=new int[26];
