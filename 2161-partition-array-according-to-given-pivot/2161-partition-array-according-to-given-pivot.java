0d3c094fdb2c00f55bfd39f1b07a8c689208e84e
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2022-10-28 12:12:35 +0530

    Time: 5 ms (97.18%), Space: 55.3 MB (96.35%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,25 +1,37 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-       List<Integer> l=new ArrayList<>();
-         List<Integer> m=new ArrayList<>();
-        List<Integer>r=new ArrayList<>();
-        for(int i:nums){
-            if(i<pivot){
-                l.add(i);
+       int ans[]=new int[nums.length]; 
+        int p1=0,p2=nums.length-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<pivot){
+                ans[p1++]=nums[i];
             }
-            else if(i==pivot){
-                m.add(i);
+            if(nums[nums.length-i-1]>pivot){
+                ans[p2--]=nums[nums.length-i-1];
             }
-            else r.add(i);
-        }
-        l.addAll(m);
-        l.addAll(r);
-        int ans[]=new int[nums.length];
-        int p=0;
-        for(int i:l){
-            ans[p]=i;
-            p++;
-        }
+        }                                            
+         while(p1<=p2){
+             ans[p1++]=pivot;
+         }                                           
+       // List<Integer> l=new ArrayList<>();
+       //   List<Integer> m=new ArrayList<>();
+       //  List<Integer>r=new ArrayList<>();
+       //  for(int i:nums){
+       //      if(i<pivot){
+       //          l.add(i);
+       //      }
+       //      else if(i==pivot){
+       //          m.add(i);
+       //      }
+       //      else r.add(i);
+       //  }
+       //  l.addAll(m);
+       //  l.addAll(r);
+       //  int p=0;
+       //  for(int i:l){
+       //      ans[p]=i;
+       //      p++;
+       //  }
         return ans;
     }
 }
