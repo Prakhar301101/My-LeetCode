30724d8af6d88b89938a6d397153c30386c1cb65
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2023-04-27 17:47:08 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //  static int dp[][];
+    //  public Solution(){
+    //      dp=new int[1002][1002];
+    //      for(int a[]:dp) Arrays.fill(a,-1);
+    //  }
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int w, int wt[], int val[], int n) 
+    { 
+        int dp[][]=new int[n+1][w+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<w+1;j++){
+                if(i==0||j==0) dp[i][j]=0;
+                //replace n=>i and w=>j
+                else if(wt[i-1]<=j){
+                    dp[i][j]=Math.max((val[i-1]+dp[i-1][j-wt[i-1]]),dp[i-1][j]);
+                }
+                else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][w];
+        
+        
+        
+        // if(n==0||w==0) return 0;
+        // else if(dp[n][w]!=-1) return dp[n][w];
+        // else if(wt[n-1]<=w){
+        //     return dp[n][w]=Math.max((val[n-1]+knapSack(w-wt[n-1],wt,val,n-1)),(knapSack(w,wt,val,n-1)));
+        // }
+        //  return dp[n][w]=knapSack(w,wt,val,n-1);
+    } 
+}
+
+
