5ea27e4d9d91f637eb0037dfafff2a269e517956
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2023-07-18 21:49:48 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.java
@@ -38,6 +38,7 @@ class Solution
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
                 if(i==0||j==0) dp[i][j]=0;
+                //i!=j otherwise all index will be common so ans would be whole string
                 else if(s.charAt(i-1)==t.charAt(j-1)&&i!=j){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
